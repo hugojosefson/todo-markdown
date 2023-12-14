1345f8eb45b54433d13eeeffd46f6b87b520d448
Hugo Josefson <hugo@josefson.org>
2023-12-14 23:39:54 +0100

    feat: add identifier to new tasklist items

--- src/transform-ast.ts
+++ src/transform-ast.ts
@@ -2,40 +2,57 @@ import { Nodes, Text } from "npm:@types/mdast";
 import { selectAll } from "npm:unist-util-select";
 
 export function transformAst(tree: Nodes): Nodes {
-  const headingTexts = selectAll("heading > text", tree) as Text[];
-  const headingTextsWithBoxes = headingTexts.filter((headingText) => {
-    return isWithBox(headingText.value);
-  });
-  const headingTextsWithBoxesAndIdentifiers = headingTexts.filter(
-    (headingText) => {
-      return isWithBoxAndIdentifier(headingText.value);
-    },
+  const texts = selectAll("heading > text:first-child", tree) as Text[];
+  const textsWithBoxes = texts.filter((text) => isWithBox(text.value));
+  const textsWithBoxesAndIdentifiers = texts.filter(
+    (text) => isWithBoxAndIdentifier(text.value),
   );
-  const headingTextsWithBoxesButNoIdentifiers = headingTextsWithBoxes.filter(
-    (headingText) => {
-      return !isWithBoxAndIdentifier(headingText.value);
-    },
+  const textsWithBoxesButNoIdentifiers = textsWithBoxes.filter(
+    (text) => !isWithBoxAndIdentifier(text.value),
   );
 
-  const identifierNumbers: number[] = headingTextsWithBoxesAndIdentifiers
-    .map((headingText) => BOX_IDENTIFIER_REGEX.exec(headingText.value)![2])
+  const listItemTexts = selectAll(
+    "listItem > paragraph:first-child text:first-child",
+    tree,
+  ) as Text[];
+  const listItemTextsWithIdentifiers = listItemTexts.filter(
+    (text) => isWithIdentifier(text.value),
+  );
+  const listItemTextsWithoutIdentifiers = listItemTexts.filter(
+    (text) => !isWithIdentifier(text.value),
+  );
+
+  const identifierNumbers: number[] = [
+    ...textsWithBoxesAndIdentifiers
+      .map((text) => BOX_IDENTIFIER_REGEX.exec(text.value)![2]),
+    ...listItemTextsWithIdentifiers
+      .map((text) => IDENTIFIER_REGEX.exec(text.value)![1]),
+  ]
     .map((identifierNumber) => Number(identifierNumber));
   let maxIdentifierNumber = Math.max(0, ...identifierNumbers);
 
-  for (const headingText of headingTextsWithBoxesButNoIdentifiers) {
+  for (const text of textsWithBoxesButNoIdentifiers) {
     const [_, box, __, rest] = BOX_REGEX.exec(
-      headingText.value,
+      text.value,
     )! as unknown as [
       string,
       Box,
       string,
       string,
     ];
-    // Add an identifier to the heading text, right after the box.
+    // Add an identifier to the text value, right after the box.
     maxIdentifierNumber += 1;
     const identifierNumber = maxIdentifierNumber;
     const identifier: Identifier = `TODO-${identifierNumber}`;
-    headingText.value = `${box} ${identifier} ${rest}`;
+    text.value = `${box} ${identifier} ${rest}`;
+  }
+
+  for (const text of listItemTextsWithoutIdentifiers) {
+    // Add an identifier to the beginning of the text value
+    maxIdentifierNumber += 1;
+    const identifierNumber = maxIdentifierNumber;
+    const identifier: Identifier = `TODO-${identifierNumber}`;
+    text.value = `${identifier} ${text.value}`;
   }
 
   return tree;
@@ -43,14 +60,19 @@ export function transformAst(tree: Nodes): Nodes {
 
 const BOX_REGEX = /^(\[[ x]\])( (.*))?$/iu;
 const BOX_IDENTIFIER_REGEX = /^(\[[ x]\]) TODO-(\d+)(.*)$/iu;
+const IDENTIFIER_REGEX = /^TODO-(\d+)(.*)$/iu;
 type Box = "[ ]" | "[x]" | "[X]";
 type Identifier = `TODO-${number}`;
 type BoxAndIdentifier = `${Box} ${Identifier}`;
 type WithBox = `${Box}${string}`;
 type WithBoxAndIdentifier = `${BoxAndIdentifier}${string}`;
+type WithIdentifier = `${Identifier}${string}`;
 export function isWithBox(s: string): s is WithBox {
   return BOX_REGEX.test(s);
 }
 export function isWithBoxAndIdentifier(s: string): s is WithBoxAndIdentifier {
   return BOX_IDENTIFIER_REGEX.test(s);
+}
+export function isWithIdentifier(s: string): s is WithIdentifier {
+  return IDENTIFIER_REGEX.test(s);
 }
