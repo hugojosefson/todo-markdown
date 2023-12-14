b14617bc27cf3cad175923a6e8a8e3f350046193
Hugo Josefson <hugo@josefson.org>
2023-12-14 23:47:53 +0100

    fix: test

--- test/transform-markdown.test.ts
+++ test/transform-markdown.test.ts
@@ -9,7 +9,7 @@ Deno.test("transformMarkdown should assign identifiers to new tasks", async () =
 - [ ] TODO-? Implement feature B
 - [ ] TODO-? Implement feature C
 - [ ] Implement feature D
-`;
+`.trim();
 
   const expectedOutputMarkdown = `
 # Project
@@ -18,7 +18,7 @@ Deno.test("transformMarkdown should assign identifiers to new tasks", async () =
 - [ ] TODO-2 Implement feature B
 - [ ] TODO-3 Implement feature C
 - [ ] TODO-4 Implement feature D
-`;
+`.trim();
 
   const result = await transformMarkdown(inputMarkdown);
   assertEquals(result, expectedOutputMarkdown);
@@ -31,7 +31,7 @@ Deno.test("transformMarkdown should not change identifiers of old tasks", async 
 - [ ] TODO-1 Implement feature A
 - [ ] TODO-2 Implement feature B
 - [ ] TODO-3 Implement feature C
-`;
+`.trim();
 
   const expectedOutputMarkdown = `
 # Project
@@ -39,21 +39,51 @@ Deno.test("transformMarkdown should not change identifiers of old tasks", async 
 - [ ] TODO-1 Implement feature A
 - [ ] TODO-2 Implement feature B
 - [ ] TODO-3 Implement feature C
-`;
+`.trim();
 
   const result = await transformMarkdown(inputMarkdown);
   assertEquals(result, expectedOutputMarkdown);
 });
 
 Deno.test("transformMarkdown should handle examples in the README", async () => {
-  const readme = await Deno.readTextFile(
-    new URL("../README.md", import.meta.url),
-  );
-  const exampleSection = readme.split(/^## .*example/i)[1].split(/^## /)[0];
-  const inputExample =
-    exampleSection.split(/^### .*input/i)[1].split(/^### /)[0];
-  const expectedOutputExample =
-    exampleSection.split(/^### .*output/i)[1].split(/^### /)[0];
+  const inputExample = `# My TODO:s
+
+## Urgent
+
+- [x] TODO-2 Pay bills
+- [ ] TODO-1 Buy milk
+- [x] TODO-3 Call mom
+- [ ] TODO-5 Buy eggs
+
+## Later
+
+- [ ] TODO-? Buy bread
+- [ ] TODO-7 Eat it all
+
+## Other
+
+- [ ] Do something else
+- [ ] TODO-xx Do something even elser
+`.trim();
+  const expectedOutputExample = `# My TODO:s
+
+## Urgent
+
+- [x] TODO-2 Pay bills
+- [ ] TODO-1 Buy milk
+- [x] TODO-3 Call mom
+- [ ] TODO-5 Buy eggs
+
+## Later
+
+- [ ] TODO-8 Buy bread
+- [ ] TODO-7 Eat it all
+
+## Other
+
+- [ ] TODO-9 Do something else
+- [ ] TODO-10 Do something even elser
+`.trim();
   const result = await transformMarkdown(inputExample);
   assertEquals(result, expectedOutputExample);
 });
