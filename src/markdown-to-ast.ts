a671dbf85e261021f58f2e84673f1431defebf2f
Hugo Josefson <hugo@josefson.org>
2023-12-16 15:16:38 +0100

    feat: remove position

--- src/markdown-to-ast.ts
+++ src/markdown-to-ast.ts
@@ -2,6 +2,25 @@ import { Nodes } from "npm:@types/mdast";
 import { fromMarkdown } from "npm:mdast-util-from-markdown";
 import { gfmFromMarkdown } from "npm:mdast-util-gfm";
 import { gfm } from "npm:micromark-extension-gfm";
+import * as R from "npm:ramda";
+
+/**
+ * Clone an object, excluding "position" properties
+ */
+const cloneWithoutPosition = R.compose(R.omit(["position"]), R.clone);
+
+/**
+ * Clone an AST "position" properties
+ */
+export function removePosition(node: unknown) {
+  if (R.is(Array, node)) {
+    return R.map(removePosition, node);
+  }
+  if (R.is(Object, node)) {
+    return R.map(removePosition, cloneWithoutPosition(node));
+  }
+  return node;
+}
 
 export function markdownToAst(markdown: string): Nodes {
   return fromMarkdown(markdown, {
@@ -14,6 +33,7 @@ export function markdownToAst(markdown: string): Nodes {
 if (import.meta.main) {
   const inputMarkdown = await Deno.readTextFile("/dev/stdin");
   const ast = markdownToAst(inputMarkdown);
-  const output = JSON.stringify(ast, null, 2);
+  const withoutPosition = removePosition(ast);
+  const output = JSON.stringify(withoutPosition, null, 2);
   console.log(output);
 }
