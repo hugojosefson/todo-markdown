50a9ca3de1e9802ced75a4dd5711c27e0a1e625e
Hugo Josefson <hugo@josefson.org>
2023-12-16 14:55:57 +0100

    wip: transform-ast.ts

--- src/transform-ast.ts
+++ src/transform-ast.ts
@@ -1,6 +1,42 @@
 import { Nodes, Text } from "npm:@types/mdast";
 import { selectAll } from "npm:unist-util-select";
 
+export function getAllTextsWithIdentifiers(
+  tree: Nodes,
+): (Text & { value: WithOnlyIdentifier })[] {
+  return selectAll("text:first-child", tree)
+    .filter((text): text is Text & { value: WithOnlyIdentifier } =>
+      isWithOnlyIdentifier(text.value)
+    );
+}
+
+/**
+ * Replaces all placeholder identifiers in the given tree with new identifiers,
+ * starting from the maximum identifier number in the given tree.
+ * @param tree
+ */
+export function transformPlaceholderIdentifiers(tree: Nodes): Nodes {
+  let maxIdentifierNumber = getMaxIdentifierNumber(tree);
+  const texts: (Text & { value: WithOnlyIdentifier })[] =
+    getAllTextsWithIdentifiers(tree);
+
+  return tree; // TODO: TODO-XX → TODO-${++maxIdentifierNumber}
+}
+
+/**
+ * Finds the maximum identifier number in the given tree.
+ * @param tree The tree to search.
+ * @returns The maximum identifier number in the given tree.
+ */
+export function getMaxIdentifierNumber(tree: Nodes): number {
+  const texts: Text[] = getAllTexts(tree);
+  return texts
+    .filter((text) => isWithOnlyIdentifier(text.value))
+    .map((text) => ONLY_IDENTIFIER_REGEX.exec(text.value)![1])
+    .map((identifierNumber) => Number(identifierNumber))
+    .reduce((max, identifierNumber) => Math.max(max, identifierNumber), 0);
+}
+
 export function transformAst(tree: Nodes): Nodes {
   const texts = selectAll("heading > text:first-child", tree) as Text[];
   const textsWithBoxes = texts.filter((text) => isWithBox(text.value));
@@ -16,17 +52,17 @@ export function transformAst(tree: Nodes): Nodes {
     tree,
   ) as Text[];
   const listItemTextsWithIdentifiers = listItemTexts.filter(
-    (text) => isWithIdentifier(text.value),
+    (text) => isWithOnlyIdentifier(text.value),
   );
   const listItemTextsWithoutIdentifiers = listItemTexts.filter(
-    (text) => !isWithIdentifier(text.value),
+    (text) => !isWithOnlyIdentifier(text.value),
   );
 
   const identifierNumbers: number[] = [
     ...textsWithBoxesAndIdentifiers
       .map((text) => BOX_IDENTIFIER_REGEX.exec(text.value)![2]),
     ...listItemTextsWithIdentifiers
-      .map((text) => IDENTIFIER_REGEX.exec(text.value)![1]),
+      .map((text) => ONLY_IDENTIFIER_REGEX.exec(text.value)![1]),
   ]
     .map((identifierNumber) => Number(identifierNumber));
   let maxIdentifierNumber = Math.max(0, ...identifierNumbers);
@@ -60,19 +96,23 @@ export function transformAst(tree: Nodes): Nodes {
 
 const BOX_REGEX = /^(\[[ x]\])( (.*))?$/iu;
 const BOX_IDENTIFIER_REGEX = /^(\[[ x]\]) TODO-(\d+)(.*)$/iu;
-const IDENTIFIER_REGEX = /^TODO-(\d+)(.*)$/iu;
+const ONLY_IDENTIFIER_REGEX = /^TODO-(\d+)(.*)$/iu;
+const WITH_MAYBE_BOX_AND_IDENTIFIER_REGEX =
+  /^(\[[ x]\] )?TODO-(\d+)( (.*))?$/iu;
+const WITH_MAYBE_BOX_AND_PLACEHOLDER_IDENTIFIER_REGEX =
+  /^(\[[ x]\] )?TODO-(\?+|x+|X+|n+|N+)( (.*))?$/iu;
 type Box = "[ ]" | "[x]" | "[X]";
 type Identifier = `TODO-${number}`;
 type BoxAndIdentifier = `${Box} ${Identifier}`;
 type WithBox = `${Box}${string}`;
 type WithBoxAndIdentifier = `${BoxAndIdentifier}${string}`;
-type WithIdentifier = `${Identifier}${string}`;
+type WithOnlyIdentifier = `${Identifier}${string}`;
 export function isWithBox(s: string): s is WithBox {
   return BOX_REGEX.test(s);
 }
 export function isWithBoxAndIdentifier(s: string): s is WithBoxAndIdentifier {
   return BOX_IDENTIFIER_REGEX.test(s);
 }
-export function isWithIdentifier(s: string): s is WithIdentifier {
-  return IDENTIFIER_REGEX.test(s);
+export function isWithOnlyIdentifier(s: string): s is WithOnlyIdentifier {
+  return ONLY_IDENTIFIER_REGEX.test(s);
 }
